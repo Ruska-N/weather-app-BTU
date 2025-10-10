289fcb8d7c10c5aadc148d82c87e6465ef0bebc3
Mariam <mariebukhaidze@gmail.com>
2025-10-10 15:36:42 +0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,54 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
+import {
+  HeaderComponent,
+  UnitSettings,
+} from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
 import { DailyForecastComponent } from './components/daily-forecast/daily-forecast.component';
 import { HourlyForecastComponent } from './components/hourly-forecast/hourly-forecast.component';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     SearchBarComponent,
     CurrentWeatherComponent,
     DailyForecastComponent,
-    HourlyForecastComponent
+    HourlyForecastComponent,
   ],
   template: `
-    <app-header></app-header>
-    <app-search-bar></app-search-bar>
-    <app-current-weather></app-current-weather>
-    <app-daily-forecast></app-daily-forecast>
-    <app-hourly-forecast></app-hourly-forecast>
+    <app-header (settingsChanged)="onSettingsChange($event)"></app-header>
+    <main class="container">
+      <app-search-bar (search)="handleSearch($event)"></app-search-bar>
+      <app-current-weather></app-current-weather>
+      <app-daily-forecast></app-daily-forecast>
+      <app-hourly-forecast></app-hourly-forecast>
+    </main>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  currentSettings: UnitSettings = {
+    temperature: 'celsius',
+    wind: 'kmh',
+    precipitation: 'mm',
+  };
+
+  currentWeather: any = null;
+
+  constructor(private weatherService: WeatherService) {}
+
+  onSettingsChange(settings: UnitSettings) {
+    this.currentSettings = settings;
+  }
+
+  handleSearch(city: string) {
+    this.weatherService.getWeatherForCity(city).subscribe((data: any) => {
+      this.currentWeather = data;
+    });
+  }
+}
--- src/app/components/hourly-forecast/hourly-forecast.component.ts
+++ src/app/components/hourly-forecast/hourly-forecast.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UnitSettings } from '../header/header.component';
+
+export interface HourlyForecast {
+  time: string;
+  icon: string;
+  temperature: {
+    celsius: number;
+    fahrenheit: number;
+  };
+}
 
 @Component({
   selector: 'app-hourly-forecast',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hourly-forecast.component.html',
-  styleUrl: './hourly-forecast.component.css'
+  styleUrls: ['./hourly-forecast.component.css'],
 })
 export class HourlyForecastComponent {
+  @Input() hourlyData: HourlyForecast[] = [];
+  @Input() settings: UnitSettings = {
+    temperature: 'celsius',
+    wind: 'kmh',
+    precipitation: 'mm',
+  };
 
+  @Output() dayChanged = new EventEmitter<string>();
+
+  isDropdownOpen = false;
+  days = [
+    'Today',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+  selectedDay = 'Today';
+
+  constructor(private elementRef: ElementRef) {}
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectDay(day: string): void {
+    this.selectedDay = day;
+    this.isDropdownOpen = false;
+    this.dayChanged.emit(day);
+  }
+
+  @HostListener('window:click', ['$event'])
+  onWindowClick(event: Event): void {
+    const dropdownWrapper = this.elementRef.nativeElement.querySelector(
+      '.day-selector-wrapper'
+    );
+    if (
+      this.isDropdownOpen &&
+      dropdownWrapper &&
+      !dropdownWrapper.contains(event.target)
+    ) {
+      this.isDropdownOpen = false;
+    }
+  }
 }
