71f33a7394c9ffc06ee0e0c040e723dd57933ad1
Ruska-N <ninidze.rusudan@gmail.com>
2025-10-10 18:21:25 +0400

    m

--- src/app/components/current-weather/current-weather.component.ts
+++ src/app/components/current-weather/current-weather.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-current-weather',
-  imports: [],
   templateUrl: './current-weather.component.html',
-  styleUrl: './current-weather.component.css'
+  styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent {
-
+  
 }
